d33e3e0485621f37a6acd42062980e3a2990febe
Mickey Krasilnikov <mickey.krasilnikov@gmail.com>
2023-02-14 17:12:58 -0800

    flipping the flag to use api instead of mocks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,7 @@ import packageJson from "../../package.json";
 
 export const environment = {
   production: true,
-  useMocks: true,
+  useMocks: false,
   environmentName: 'PROD',
-  apiBaseUrl: 'TBD',
   version: packageJson.version,
 };
