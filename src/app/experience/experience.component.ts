f7f52bed9ec786bf490d581a594aa32dece230b8
Mickey Krasilnikov <mickey.krasilnikov@gmail.com>
2023-03-12 14:10:25 -0700

    improvements in experience component

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -22,36 +22,6 @@ export class ExperienceComponent implements OnInit {
         nextEl: '.next',
         prevEl: '.prev',
       },
-      on: {
-        init: (swiper: Swiper) => {
-          var target = document
-            .querySelectorAll('.exprns-slider__item')
-            [swiper.activeIndex].getAttribute('data-target');
-
-          var imageElements = document.querySelectorAll('.exprns-img__item');
-          for (let i = 0; i < imageElements.length; i++) {
-            if (imageElements[i].id === target) {
-              this.renderer.addClass(imageElements[i], 'active');
-            } else {
-              this.renderer.removeClass(imageElements[i], 'active');
-            }
-          }
-        },
-        slideChange: (swiper: Swiper) => {
-          var target = document
-            .querySelectorAll('.exprns-slider__item')
-            [swiper.activeIndex].getAttribute('data-target');
-
-          var imageElements = document.querySelectorAll('.exprns-img__item');
-          for (let i = 0; i < imageElements.length; i++) {
-            if (imageElements[i].id === target) {
-              this.renderer.addClass(imageElements[i], 'active');
-            } else {
-              this.renderer.removeClass(imageElements[i], 'active');
-            }
-          }
-        },
-      },
     });
   }
 }
