24783965b88c344f6dc63745befb4d6674c6a129
Mickey Krasilnikov <mickey.krasilnikov@gmail.com>
2023-03-20 23:06:51 -0700

    improvements in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  showMenu = false;
 
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+    private elementRef: ElementRef
+  ) {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  handleClick(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.showMenu = false;
+    }
+  }
+  ngOnInit(): void {
+    this.renderer.listen(this.document, 'click', (event: Event) => {
+      this.handleClick(event);
+    });
+  }
 }
